90d95000b637b317c55fa083048d8de5fca2e3b4
Alastair Smith <j.alastair.smith@outlook.com>
2021-07-25 13:31:41 +0100

    adds flight class and tests, finishes mvp

--- src/main/java/planes/Flight.java
+++ src/main/java/planes/Flight.java
@@ -1,4 +1,81 @@
 package planes;
 
+import people.FlightAttendant;
+import people.Passenger;
+import people.Pilot;
+
+import java.util.ArrayList;
+
 public class Flight {
+
+    private Pilot pilot;
+    private ArrayList<FlightAttendant> cabinCrew;
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destination;
+    private String origin;
+    private String departureTime;
+
+    public Flight(Pilot pilot, Plane plane, String flightNumber, String destination, String origin, String departureTime) {
+        this.pilot = pilot;
+        this.cabinCrew = new ArrayList<>();
+        this.passengers = new ArrayList<>();
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.origin = origin;
+        this.departureTime = departureTime;
+    }
+
+    public Pilot getPilot() {
+        return pilot;
+    }
+
+    public ArrayList<FlightAttendant> getCabinCrew() {
+        return cabinCrew;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public int getEmptySeats(){
+        int totalSeats = this.getPlane().getPlaneType().getCapacity();
+        int takenSeats = this.passengers.size();
+        int emptySeats = totalSeats - takenSeats;
+        return emptySeats;
+    }
+
+    public void addCrewMember(FlightAttendant flightAttendant){
+        cabinCrew.add(flightAttendant);
+    }
+
+    public void bookPassenger(Passenger passenger){
+        int flightPassengerCapacity = this.getPlane().getPlaneType().getCapacity();
+        if(this.passengers.size() < flightPassengerCapacity){
+            passengers.add(passenger);
+        }
+    }
+
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,64 @@
+import org.junit.Before;
+import org.junit.Test;
+import people.FlightAttendant;
+import people.Passenger;
+import people.Pilot;
+import planes.Flight;
+import planes.Plane;
+import planes.PlaneType;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Flight flight;
+    Plane plane;
+    Pilot pilot;
+    Passenger passenger;
+    Passenger passenger2;
+    FlightAttendant cabinCrew;
+
+    @Before
+    public void before(){
+        plane = new Plane(PlaneType.CESSNA172);
+        pilot = new Pilot("Adama", "BG18102004");
+        flight = new Flight(pilot, plane, "BG124", "NCP", "JFK)", "10:30");
+        passenger = new Passenger("Number Six", 2);
+        passenger2 = new Passenger("Number One", 2);
+        cabinCrew = new FlightAttendant("Gaius");
+        flight.bookPassenger(passenger);
+        flight.bookPassenger(passenger2);
+        flight.addCrewMember(cabinCrew);
+    }
+
+    @Test
+    public void canGetPlaneType(){
+        assertEquals(PlaneType.CESSNA172, flight.getPlane().getPlaneType());
+    }
+
+    @Test
+    public void getEmptySeats(){
+        assertEquals(3, flight.getEmptySeats());
+    }
+
+    @Test
+    public void canAddPassenger(){
+        Passenger passenger3 = new Passenger("Number Two", 2);
+        flight.bookPassenger(passenger3);
+        assertEquals(3,flight.getPassengers().size());
+    }
+
+    @Test
+    public void cannotAddPassengerIfPlaneIsFull(){
+        //plane capacity is 4, passenger5 should not be added
+        Passenger passenger3 = new Passenger("Number Two", 2);
+        Passenger passenger4 = new Passenger("Number Three", 2);
+        Passenger passenger5 = new Passenger("Number Four", 2);
+        flight.bookPassenger(passenger3);
+        flight.bookPassenger(passenger4);
+        flight.bookPassenger(passenger5);
+        assertEquals(4, flight.getPassengers().size());
+    }
 }
+
+
